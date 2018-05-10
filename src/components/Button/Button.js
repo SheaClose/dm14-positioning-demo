4d6ac4f288fa1ef473e609a3e01c0a634152fa52
SheaClose <SheaClose@gmail.com>
2018-05-10 12:55:09 -0500

    done

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Button.css";
 
 export default () => (
   <button className="btn" type="button" name="button">
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ham from "../../static/ham.png";
+import "./NavBar.css";
+
+// const nav = {
+//   height: "15vh",
+//   display: "flex",
+//   justifyContent: "space-between"
+// };
+const styles = {
+  nav: {
+    height: "15vh",
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  img: {
+    width: "40px"
+  }
+};
 
 export default () => (
-  <nav>
-    <img src={ham} className="ham" height="50" alt="hamburger icon" />
+  <nav style={styles.nav}>
+    <img style={styles.img} src={ham} className="ham" alt="hamburger icon" />
     <ul className="links-container">
       <li className="link">Home</li>
       <li className="link">Products</li>
